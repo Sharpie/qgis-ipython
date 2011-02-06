7b1965badf87aa4642f302ddac349ed235247efb
Charlie Sharpsteen <source@sharpsteen.net>
2011-02-06 01:06:33 -0800

    Initial commit of files generated by Plugin Builder

---
+++ __init__.py
@@ -0,0 +1,35 @@
+"""
+/***************************************************************************
+ ipython
+                                 A QGIS plugin
+ An enhanced QGIS console powered by IPython
+                             -------------------
+        begin                : 2011-02-06
+        copyright            : (C) 2011 by Charlie Sharpsteen
+        email                : source@sharpsteen.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+def name():
+    return "IPython Console"
+def description():
+    return "An enhanced QGIS console powered by IPython"
+def version():
+    return "Version 0.1"
+def icon():
+    return "icon.png"
+def qgisMinimumVersion():
+    return "1.6"
+def classFactory(iface):
+    # load ipython class from file ipython
+    from ipython import ipython
+    return ipython(iface)
